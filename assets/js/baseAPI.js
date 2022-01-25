6678863bbd20e2d94f4d412d673a06f843964985
A_ye <1464102511@qq.com>
2022-01-25 19:54:10 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,27 @@ $.ajaxPrefilter(function(options) {
     // 发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
         // console.log(options.url); // http://www.liulongbin.top:3007/api/login
+        // console.log(options);
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数
+    // 无论成功还是失败，最终都会调用 complete 回调函数
+    options.complete = function(res) {
+        // console.log('执行了complete 回调函数');
+        // console.log(res);
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        // console.log(res.responseJSON); // {status: 1, message: "身份认证失败！"}
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1. 强制清空 token
+            localStorage.removeItem('token')
+                // 2. 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
